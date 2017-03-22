fe8d7749bd5b6acd05511befc258e8988160eae3
oacastillol <osmar.alejandro.castillo@gmail.com>
2017-03-22 01:28:26 +0000

    modelos generados

---
+++ db/migrate/20170322012607_create_employees.rb
@@ -0,0 +1,24 @@
+class CreateEmployees < ActiveRecord::Migration
+  def change
+    create_table :employees do |t|
+      t.string :document_type
+      t.string :document_number
+      t.string :first_name
+      t.string :other_name
+      t.string :last_name
+      t.string :second_surname
+      t.date :birthdate
+      t.string :birthplace
+      t.string :address
+      t.string :phones
+      t.string :email
+      t.date :admission_date
+      t.date :retirement_date
+      t.decimal :salary
+      t.boolean :transport_aid
+      t.boolean :integral_salary
+
+      t.timestamps null: false
+    end
+  end
+end
