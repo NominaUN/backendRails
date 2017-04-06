0ef65749ca96ff2462cdc5e449d91ed64dcf193f
Diego Salgado <dasalgadob@unal.edu.co>
2017-04-05 22:58:41 -0500

    Se arreglan validaciones para poder hacer setup inicial de base de datos

--- app/models/general_parameter.rb
+++ app/models/general_parameter.rb
@@ -1,6 +1,6 @@
 class GeneralParameter < ApplicationRecord
 	
-  validates :round, :laboral_days, :payday, :integral_base, presence: true
+  validates :round_type, :laboral_days, :payday, :integral_base, presence: true
 	
   def self.load_general_parameters(page=1,per_page=20)
     paginate(:page => page,:per_page => per_page)
