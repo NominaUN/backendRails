e32344fa1d42bcf02c53ab9bd809be3519e69eff
Andres Ceballos <afceballosr@unal.edu.co>
2017-04-06 15:50:25 -0500

    queries faltantes con includes, validaciones y paginacion de metodos de options, logs, users, payday_masters, payday_details y genral_parameters

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,11 +1,26 @@
 class Option < ApplicationRecord
+  has_many: logs  
+   
   default_scope {order("options.action ASC")}
   
   def self.load_options(page=1,per_page=20)
-    paginate(:page => page,:per_page => per_page)
+	include(:logs)
+		.paginate(:page => page,:per_page => per_page)
   end
   
   def self.option_by_id(id)
-    find_by_id(id)
+    include(:logs).find_by_id(id)
+  end
+  
+  def self.options_by_ids(ids, page=1, per_page=20)
+    load_options(page, per_page).where(options:{
+        id: ids
+    })
+  end
+
+  def self.options_not_by_ids(ids, page=1, per_page=20)
+    load_options(page,per_page).where.not(options: {
+        id: ids
+    })
   end
 end
