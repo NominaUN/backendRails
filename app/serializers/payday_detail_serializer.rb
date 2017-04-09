a1f1bdde71282deccbfd6e07cfe5f7d1fb1b6cec
Andrés Felipe Ceballos Rodríguez <afceballosr@unal.edu.co>
2017-04-09 16:19:02 -0500

    relaciones

--- app/serializers/payday_detail_serializer.rb
+++ app/serializers/payday_detail_serializer.rb
@@ -1,3 +1,7 @@
 class PaydayDetailSerializer < ActiveModel::Serializer
   attributes :id, :base_value, :win, :loss, :appropiation, :worked_days, :start_date, :end_date, :concept_id, :employee_id, :payday_master_id
+
+  belongs_to :concept
+  belongs_to :employee
+  belongs_to :payday_master
 end
