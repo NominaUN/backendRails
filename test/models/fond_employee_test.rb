b37f62a23a0b1e1ae8ab54df8584ba68df3fc46b
Andres Ceballos <afceballosr@unal.edu.co>
2017-04-16 21:26:20 -0500

    avance en tests

--- test/models/fond_employee_test.rb
+++ test/models/fond_employee_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class FondEmployeeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+	@fond_employee = FondEmployee.new(employee_id: 1, fond_id: 1)
+  end
+  
+  test "should be valid the fond_employee instance" do
+	assert @fond_employee.valid?
+  end	
+   
+  test "employee_id should be present" do
+	@fond_employee.employee_id = nil
+	assert_not @fond_employee.valid?
+  end
+  
+  test "fond_id should be present" do
+	@fond_employee.fond_id = nil
+	assert_not @fond_employee.valid?
+  end
 end
