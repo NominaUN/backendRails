f271a3782ffb448da7fbc8d0bcc6daf9a910cbad
Andres Ceballos <afceballosr@unal.edu.co>
2017-04-16 20:33:34 -0500

    avance en tests

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -2,20 +2,11 @@ require 'test_helper'
 
 class EmployeeTest < ActiveSupport::TestCase
 
-	def setup
-		@area = Area.new(area_name: "Technology")
-		@area.save
-		@position = Position.new(position_name: "Manager")
-		@position.save
-		
-		@employee = Employee.new(position_id: @position.id, area_id: @area.id, document_type: "CC", document_number: 123456, first_name: "Manuel",
+	def setup	
+		@employee = Employee.new(position_id: 1, area_id: 1, document_type: "CC", document_number: 123456, first_name: "Manuel",
 									other_name: "Mauricio", last_name: "Perez", second_surname: "Rodriguez", birthdate: Date.new(1990,11,22), birthplace:"Bogota",
 									address: "Cra 4 # 22", phones: "7334425", email: "maur@hotmail.com", admission_date: Date.new(2017,1,10),
 									retirement_date: Date.new(2017,7,10), salary: 1500000.0, transport_aid: true, integral_salary: false)
-		@employee.valid?
-
-
-		Rails::logger.debug "Errors in employee: #{@employee.errors.messages}"
 	end
 
   test "should be a valid record to save" do
