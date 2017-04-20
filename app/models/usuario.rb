e4cd38f76d72b81cb29d35a21a5bde868c3a110c
dfhincapiem <dfhincapiem@unal.edu.co>
2017-04-19 20:53:18 -0500

    Se añade tablas y gema para autenticacion

---
+++ app/models/usuario.rb
@@ -0,0 +1,9 @@
+class Usuario < ActiveRecord::Base
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable,
+          :omniauthable
+  include DeviseTokenAuth::Concerns::User
+  #before_save -> { skip_confirmation! }
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416230403) do
+ActiveRecord::Schema.define(version: 20170420010809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -158,6 +158,35 @@ ActiveRecord::Schema.define(version: 20170416230403) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["employee_id"], name: "index_users_on_employee_id", using: :btree
+  end
+
+  create_table "usuarios", force: :cascade do |t|
+    t.string   "provider",               default: "email", null: false
+    t.string   "uid",                    default: "",      null: false
+    t.string   "encrypted_password",     default: "",      null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,       null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "name"
+    t.string   "nickname"
+    t.string   "image"
+    t.string   "email"
+    t.json     "tokens"
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.index ["confirmation_token"], name: "index_usuarios_on_confirmation_token", unique: true, using: :btree
+    t.index ["email"], name: "index_usuarios_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true, using: :btree
+    t.index ["uid", "provider"], name: "index_usuarios_on_uid_and_provider", unique: true, using: :btree
   end
 
   create_table "vacations", force: :cascade do |t|
