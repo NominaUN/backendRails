283b80ec6cffdf7080252845c20c1ed66ad5ffe3
Diego Salgado <dasalgadob@unal.edu.co>
2017-04-21 02:44:04 -0500

    No colocar booleans en validate presence!!!

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,18 +1,19 @@
 class Option < ApplicationRecord
-  
-  validates :option_name, :insert_action, :update_action, :delete_action,  presence: true
-
+  #Booleanos no se deben dejar en el validate presence porque en ese caso solo acepta quienes tengan
+  #Valor  verdadero
+  validates :option_name,  presence: true
+  validates :insert_action, :update_action, :delete_action, inclusion: { in: [ true, false ] }
   has_many :logs
   has_many :users, through: :logs  
    
   default_scope {order("options.option_name ASC")}
   
   def self.load_options(page=1,per_page=20)
-	paginate(:page => page,:per_page => per_page)
+	includes(:logs,:users).paginate(:page => page,:per_page => per_page)
   end
   
   def self.option_by_id(id)
-    find_by_id(id)
+    includes(:logs, :users).find_by_id(id)
   end
   
   def self.options_by_ids(ids, page=1, per_page=20)
