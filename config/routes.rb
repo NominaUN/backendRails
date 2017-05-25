10aaa398be7769bfc4d09b47355f3814fbd847cb
Osmar Castillo <oacastillol@unal.edu.co>
2017-05-25 11:16:05 -0500

    protegiendo el back

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :general_parameters
-      resources :payday_details, defaults: {format: :json}
-      resources :payday_masters, defaults: {format: :json}
-
-      resources :areas
-      resources :positions
-      resources :employees
-      resources :vacations
-
-      resources :fonds
-      resources :fond_employees
-      resources :novelties
-      resources :concepts
+      authenticate :user do
+        resources :general_parameters
+        resources :payday_details, defaults: {format: :json}
+        resources :payday_masters, defaults: {format: :json}
+        
+        resources :areas
+        resources :positions
+        resources :employees
+        resources :vacations
+        
+        resources :fonds
+        resources :fond_employees
+        resources :novelties
+        resources :concepts
+      end
     end
   end
 
